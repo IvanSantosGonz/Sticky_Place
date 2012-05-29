2a02772118fc08ec7e65a58cac11d39e71627d05
Alexandra <alexandrarivero2003@hotmail.com>
2012-05-29 11:31:03 +0100

    jquery

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+/* Using multiple unit types within one animation. */
+
+  
+   $(document).ready(function(){         
+  
+   $(init);
+
+   function init()
+   {
+	   cloudMove();
+	}
+
+   function cloudMove()
+   {
+		 $("#cloud").animate({"left": "-=50px"}, "slow");
+	     cloudMove();
+   }
+    
+   }
+  
+
+ 
