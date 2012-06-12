5fdef0c9ea9cf1c07572b4acedb4e435fdaeca29
Alexandra <alexandrarivero2003@hotmail.com>
2012-06-12 23:04:06 +0100

    maquetado de follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
  	
 	def index
     @title = "All users"
-    @users = User.paginate( :page => params[:page],:per_page => 10)
+    @users = User.paginate( :page => params[:page],:per_page => 4)
   end
 
  	def show
@@ -68,14 +68,14 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @user = User.find(params[:id])
-    @users = @user.following.paginate(:page => params[:page])
+    @users = @user.following.paginate(:page => params[:page], :per_page => 4)
     render 'show_follow'
   end
 
   def followers
     @title = "Followers"
     @user = User.find(params[:id])
-    @users = @user.followers.paginate(:page => params[:page])
+    @users = @user.followers.paginate(:page => params[:page], :per_page => 4)
     render 'show_follow'
   end
   
